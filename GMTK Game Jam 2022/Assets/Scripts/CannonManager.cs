231a0a37d92e26fda361ca04f280b92ffa2239c7
GOHSGameDesign1 <GOHSGameDesign1@gmail.com>
2022-07-15 10:32:57 -0700

    Added Cannon

---
+++ GMTK Game Jam 2022/Assets/Scripts/CannonManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonManager : MonoBehaviour
+{
+    Vector2 direction;
+    Vector2 mousePos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        direction = mousePos - (Vector2)transform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, angle);
+    }
+}
