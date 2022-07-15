2943d0617fe8c8a35993a023b92e7c5d6b2ecdbb
GOHSGameDesign1 <GOHSGameDesign1@gmail.com>
2022-07-15 12:11:03 -0700

    Added Health and dicedisplay on enemy

--- GMTK Game Jam 2022/Assets/Scripts/EnemyHealth.cs
+++ GMTK Game Jam 2022/Assets/Scripts/EnemyHealth.cs
@@ -7,17 +7,23 @@ public class EnemyHealth : MonoBehaviour
 {
     public bool diceDependant;
     public float maxHealth;
+    public int diceNumber;
     GameObject textObject;
+    GameObject diceDisplayObject;
     
     // Start is called before the first frame update
     void Start()
     {
         textObject = transform.GetChild(0).gameObject;
+        diceDisplayObject = transform.GetChild(1).gameObject;
     }
 
     // Update is called once per frame
     void Update()
     {
+        textObject.SetActive(!diceDependant);
+        diceDisplayObject.SetActive(diceDependant);
         textObject.GetComponent<TextMeshPro>().text = maxHealth.ToString();
+        diceDisplayObject.GetComponent<TextMeshPro>().text = new string(diceNumber.ToString() + "D");
     }
 }
