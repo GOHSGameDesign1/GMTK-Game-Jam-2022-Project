3c574f50f998863e353289e5973d7a7d463a94da
brandon1113 <brandonsspeaker@gmail.com>
2022-07-15 23:02:12 -0700

    randomizer script pls help leon

---
+++ GMTK Game Jam 2022/Assets/Scripts/DiceRandomizer.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+
+
+public class DiceRandomizer : MonoBehaviour
+
+
+
+{
+    public GameObject dicePrefab;
+
+    int randomNumber;
+
+    void Start()
+    {
+
+    }
+
+    void RollTheDice()
+    {
+        randomNumber = int i in Enumerable.Range(1,6);
+    }
+
+}
